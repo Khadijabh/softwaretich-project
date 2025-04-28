cb10077a1f0425df887974402b9ba746387f2ee7
Khadija <khadjiabouhou7@gmail.com>
2025-04-28 22:43:31 +0100

    Troisieme version

--- Backend/user-service/src/main/java/com/softwaretich/user_service/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.softwaretich.user_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.softwaretich.user_service.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
